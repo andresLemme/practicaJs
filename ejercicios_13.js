2b35817c710d70df5da09effbc3f46c284dd6666
andresacn <andres.lemme@gmail.com>
2021-07-22 18:23:02 -0300

    funcion join()

---
+++ ejercicios_13.js
@@ -0,0 +1,12 @@
+let paises = ["Colombia", "Peru", "Argentina", "Venezuela", "Chile"]
+
+console.log(paises)
+
+console.log("----------------------")
+
+console.log(paises.toString())
+console.log(paises.join())
+console.log(paises.join(","))
+console.log(paises.join("_"))
+console.log(paises.join("-"))
+console.log(paises.join(";"))
